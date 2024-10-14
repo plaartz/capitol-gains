f915dd23a4e09dc93012ae0c5fac41f99e455bb7
Lawrence Wang <lwang724@wisc.edu>
2024-10-13 20:49:59 -0500

    add simple test fixtures for the test pipeline

--- test/conftest.py
+++ test/conftest.py
@@ -1,5 +1,19 @@
 import pytest
+from django.core.management import call_command
+from ..api.core.models import StockPrice, Stock
 
 @pytest.fixture
-def politician_fixture():
-    assert True
+@pytest.mark.django_db
+def stock_price_test(django_db_setup, django_db_blocker):
+    with django_db_blocker.unblock():
+        call_command('loaddata', 'api/api/fixtures/StockPriceFixture.json')
+    stocks_without_prices = StockPrice.objects
+    assert stocks_without_prices.count() == 35
+
+@pytest.fixture
+@pytest.mark.django_db
+def stock_test(django_db_setup, django_db_blocker):
+    with django_db_blocker.unblock():
+        call_command('loaddata', 'api/api/fixtures/StockFixture.json')
+    stocks = Stock.objects
+    assert stocks.count() == 7
