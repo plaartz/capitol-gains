e6fd5b2e3773a342ca1f7af948e826336dea1151
Lawrence Wang <lwang724@wisc.edu>
2024-10-19 16:05:02 -0500

    attempting to change pytest pipeline to unittest

--- api/conftest.py
+++ api/conftest.py
@@ -1,36 +1,47 @@
-import pytest
-from django.core.management import call_command
+import unittest
 from .core.models import StockPrice, Stock, Politician, Profile, Transaction
 
-@pytest.fixture
-def stock_price_test():
-    stock_prices = StockPrice.objects
-    assert stock_prices.count() == 35
+class StockPriceTestCase(unittest.TestCase):
+
+    def test_stock_price_count(self):
+        stock_prices = StockPrice.objects
+        self.assertEqual(stock_prices.count(), 35)
 
 
-@pytest.fixture
-def stock_test():
-    stocks = Stock.objects
-    assert stocks.count() == 7
+class StockTestCase(unittest.TestCase):
+
+    def test_stock_count(self):
+        stocks = Stock.objects
+        self.assertEqual(stocks.count(), 7)
 
 
-@pytest.fixture
-def politician_test():
-    politicians = Politician.objects
-    assert politicians.count() == 4
+class PoliticianTestCase(unittest.TestCase):
+
+    def test_politician_count(self):
+        politicians = Politician.objects
+        self.assertEqual(politicians.count(), 4)
 
 
-@pytest.fixture
-def profile_test():
-    profiles = Profile.objects
-    assert profiles.count() == 4
-    assert profiles[0]['pk'] == 1
-    assert profiles[0]['fields']['first_name'] == 'Daven'
-    assert profiles[0]['fields']['last_name'] == 'Thakkar'
-    assert profiles[0]['fields']['middle_initial'] == 'C'
+class ProfileTestCase(unittest.TestCase):
+
+    def test_profile_count(self):
+        profiles = Profile.objects.all()
+        self.assertEqual(profiles.count(), 4)
+
+    def test_profile_fields(self):
+        profile = Profile.objects.first()
+        self.assertEqual(profile.pk, 1)
+        self.assertEqual(profile.first_name, 'Daven')
+        self.assertEqual(profile.last_name, 'Thakkar')
+        self.assertEqual(profile.middle_initial, 'C')
 
 
-@pytest.fixture
-def transaction_test():
-    transactions = Transaction.objects
-    assert transactions.count() == 22
+class TransactionTestCase(unittest.TestCase):
+
+    def test_transaction_count(self):
+        transactions = Transaction.objects
+        self.assertEqual(transactions.count(), 22)
+
+
+if __name__ == '__main__':
+    unittest.main()
