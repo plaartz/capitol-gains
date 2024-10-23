421cf3dcfe31e3f92dc9e475439530f284ae18df
Lawrence Wang <lwang724@wisc.edu>
2024-10-23 12:59:02 -0500

    started creating controller to upload transactions

---
+++ api/core/controllers/transaction.py
@@ -0,0 +1,23 @@
+from datetime import datetime, timedelta
+from django.db.models import Exists, OuterRef
+
+from core.models import Transaction, Stock, StockPrice, Profile
+
+def upload_transactions(transactions: list) -> int:
+    """
+    Controller to upload transactions scraped from government website to database
+
+    @param transactions     a list of transactions reports
+    @return                 status code representing if the upload was successful or not
+    """
+    for transaction in transactions:
+        # Profile Information
+        first_name = transaction['first_name']
+        last_name = transaction['last_name']
+        middle_initial = transaction['middle_initial']
+        profile = Profile(first_name=first_name, middle_initial=middle_initial, last_name=last_name)
+        
+        # TODO: Politician Information
+
+        # TODO: Stock Information
+    pass
