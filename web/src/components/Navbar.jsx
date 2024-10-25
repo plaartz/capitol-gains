fa254a2df73d486d4da0b96baaaf0b4315838185
Nivesh Tuwani <tuwani@wisc.edu>
2024-10-25 18:48:53 -0500

    updated navbar

--- web/src/components/Navbar.jsx
+++ web/src/components/Navbar.jsx
@@ -1,15 +1,38 @@
-import { Navbar as Navigation, Nav } from 'react-bootstrap';
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, NavLink } from 'react-router-dom'
+import "../styles/Navbar.css"
+import logo from '../../../LOGO.png'
 
-function Navbar() {
-    return <Navigation bg="dark" variant="dark" sticky="top" expand="sm" collapseOnSelect>
-        <Navigation.Collapse id="responsive-navbar-nav" className="me-auto">
-          <Nav className = "me-auto">
-            <Nav.Link as={Link} to="/">About Us</Nav.Link>
-            <Nav.Link as={Link} to="/politician-stock">Politician Stock</Nav.Link>
-          </Nav>
-        </Navigation.Collapse>
-      </Navigation>
+const Navbar = () => {
+
+  const [menuOpen, setMenuOpen] = useState(false)
+
+  return (
+    <nav>
+      <div className="logo-container">
+        <Link to="/" className="logo">
+          <img src={logo} alt="Logo" className="aboutImg" />
+        </Link>
+        <Link to="/" className="title">CAPITOL GAINS</Link>
+      </div>
+      <div className='menu' onClick={() => {
+        setMenuOpen(!menuOpen);
+      }}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+        <ul className={menuOpen ? "open" : ""}> 
+          <li>
+            <NavLink to='/AboutUs'>About us</NavLink>
+          </li>
+          <li>
+            <NavLink to='/PoliticianStock'>Politician Track</NavLink>
+          </li>
+        </ul>
+    </nav>
+  )
 }
 
 export default Navbar
