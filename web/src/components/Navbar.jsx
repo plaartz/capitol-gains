902d04a6251a2dab1d85441d94ccc9419d0eac81
Nivesh Tuwani <tuwani@wisc.edu>
2024-10-25 18:57:47 -0500

    updated navbar with linting working correctly

--- web/src/components/Navbar.jsx
+++ web/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { Link, NavLink } from 'react-router-dom'
 import "../styles/Navbar.css"
 import logo from '../../../LOGO.png'
