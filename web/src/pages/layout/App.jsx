02f12567575d9e24f112899cf19193751c388af5
Nivesh Tuwani <tuwani@wisc.edu>
2024-10-25 19:10:28 -0500

    Removed redundant file and updated main and app

--- web/src/pages/layout/App.jsx
+++ web/src/pages/layout/App.jsx
@@ -1,23 +1,24 @@
-//import { useState } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import 'src/styles/App.css'
+import '../../styles/App.css'
+import Navbar from '../../components/Navbar'
+import { Routes, Route } from 'react-router-dom'
+import AboutUs from '../../components/pages/AboutUs'
+import PoliticianStock from '../../components/pages/PoliticianStock'
 
-import AboutUs from 'src/pages/AboutUs'
-import Layout from './Layout'
-import PoliticianStock from 'src/pages/PoliticianStock'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<AboutUs />}/>
-          <Route path="politician-stock" element={<PoliticianStock />}/>
-          <Route path="*" element={<AboutUs />}/>
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+    <>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<AboutUs />}/>
+          <Route path='/AboutUs' element={<AboutUs />}></Route>
+          <Route path='/PoliticianStock' element={<PoliticianStock />}></Route>
+        </Routes>
+      </div>
+    </>
+  )
 }
 
 export default App
+
