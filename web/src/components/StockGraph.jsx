9207eb4e7fa091df259f17e61e9c9a5f993d9c7b
Porter Laartz <plaartz@wisc.edu>
2024-11-05 13:13:15 -0600

    Added Reference Line for transaction date

--- web/src/components/StockGraph.jsx
+++ web/src/components/StockGraph.jsx
@@ -1,14 +1,20 @@
 import { useRef, useState } from "react";
-import { LineChart, ResponsiveContainer, XAxis, YAxis, Line } from 'recharts'
+import { LineChart, ResponsiveContainer, XAxis, YAxis, Line, ReferenceLine } from 'recharts'
 
-export default function StockGraph({ticker, data}) {
+export default function StockGraph({ticker, data, type, transaction = false, transaction_details={}}) {
     return (
       <ResponsiveContainer width="100%" height="100%" aspect={2}>
         <LineChart data={data}>
-
+          {transaction ? (
+            <>
+              <ReferenceLine x={transaction_details.transaction_date} label="Transaction Date"/>
+            </>
+          ) : (
+            <></>
+          )}
           <XAxis dataKey="date" />
           <YAxis />
-          <Line type="linear" dataKey="price" dot={false}/>
+          <Line type="linear" dataKey="price" dot={false} />
         </LineChart>
       </ResponsiveContainer>
     );
