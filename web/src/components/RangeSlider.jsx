f9664cd43397d4bd79d76861278d12578a9bcf83
Lawrence Wang <lwang724@wisc.edu>
2024-11-19 03:32:13 -0600

    fix linting issues

--- web/src/components/RangeSlider.jsx
+++ web/src/components/RangeSlider.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import Slider from 'react-slider';
 import "./styles/RangeSlider.css";
 
--- web/src/components/SearchTools.jsx
+++ web/src/components/SearchTools.jsx
@@ -64,7 +64,8 @@ export default function SearchTools() {
     minPrice,
     maxPrice,
     purchaseSelected,
-    saleSelected
+    saleSelected,
+    setFilters
   ])
 
   return (
