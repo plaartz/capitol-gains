45fb6b50f24be34c2726416227bf7eeee916f892
Nivesh Tuwani <tuwani@wisc.edu>
2024-11-25 10:34:10 -0600

    removed comments

--- web/src/components/Pagination.jsx
+++ web/src/components/Pagination.jsx
@@ -14,7 +14,6 @@ const Pagination = ({ totalPosts, pageSize, currPageNo, paginate }) => {
       );
       const endPage = Math.min(totalPages - 1, startPage + maxVisiblePages - 1);
 
-      // Add the pages to display
       for (let i = startPage; i <= endPage; i++) {
         pages.push(i);
       }
@@ -27,7 +26,6 @@ const Pagination = ({ totalPosts, pageSize, currPageNo, paginate }) => {
   return (
     <nav>
       <ul className="pagination justify-content-center">
-        {/* Previous Button */}
         <li className={`page-item ${currPageNo === 1 ? "disabled" : ""}`}>
           <button
             className="page-link"
@@ -38,21 +36,18 @@ const Pagination = ({ totalPosts, pageSize, currPageNo, paginate }) => {
           </button>
         </li>
 
-        {/* First Page */}
         <li className={`page-item ${currPageNo === 1 ? "active" : ""}`}>
           <button onClick={() => paginate(1)} className="page-link">
             1
           </button>
         </li>
 
-        {/* Ellipsis for Pages Before */}
         {pageNo[0] > 2 && (
           <li className="page-item disabled">
             <span className="page-link">...</span>
           </li>
         )}
 
-        {/* Dynamic Pages */}
         {pageNo.map((number) => (
           <li
             key={number}
@@ -64,14 +59,12 @@ const Pagination = ({ totalPosts, pageSize, currPageNo, paginate }) => {
           </li>
         ))}
 
-        {/* Ellipsis for Pages After */}
         {pageNo[pageNo.length - 1] < totalPages - 1 && (
           <li className="page-item disabled">
             <span className="page-link">...</span>
           </li>
         )}
 
-        {/* Last Page */}
         {totalPages > 1 && (
           <li className={`page-item ${currPageNo === totalPages ? "active" : ""}`}>
             <button onClick={() => paginate(totalPages)} className="page-link">
@@ -80,7 +73,6 @@ const Pagination = ({ totalPosts, pageSize, currPageNo, paginate }) => {
           </li>
         )}
 
-        {/* Next Button */}
         <li className={`page-item ${currPageNo === totalPages ? "disabled" : ""}`}>
           <button
             className="page-link"
